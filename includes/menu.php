eb862b405622ecdb18b058fe3baa6ebda953ee24
Kratos237 <danieluokof@gmail.com>
2020-05-30 15:43:55 +0100

    Interface de la page d'acceuil

--- includes/menu.php
+++ includes/menu.php
@@ -1,15 +1,15 @@
 
-        <div class="ui menu inverted vertical dark" style="padding:60px 10px;" id="mainmenu">
+        <div class="ui menu two wide column inverted vertical dark" style="padding:60px 10px;" id="mainmenu">
             <div class="ui container">
                 <a class="active item teal">Acceuil</a>
                 <a class="item teal">Documents</a>
                 <a class="item teal">Clients</a>
                 <div class="item teal">
-                    <a href="../traitements/deconnexion.php" class="ui primary button" style="width:150px"><i
+                    <a href="../traitements/deconnexion.php" class="ui primary button"><i
                             class="ui log out icon"></i>Deconnexion</a>
                 </div>
                 <div class="item teal">
-                    <a href="#" onclick="offer()" class="ui button red" style="width:150px"><i
+                    <a href="#" onclick="offer()" class="ui button red"><i
                             class="ui shutdown icon"></i> Eteindre</a>
                 </div>
             </div>
--- pages/interface/index.php
+++ pages/interface/index.php
@@ -17,26 +17,39 @@
         <?php 
             include("../../includes/menu.php");
         ?>
-        <div class="ui column">
-            <div class="ui container" >
-                sfwreftef
+
+        <div class="ui thirteen wide column center aligned" style="margin-top: 100px;">
+            <h2 class="ui blue image header">
+                Bienvenue <span class="greating"><?php echo $_SESSION['login'] ?></span>
+                <br>
+                <br>
+                <img src="../../images/workspace1_122059.png" style="width:200px;" class="image">
+            </h2>
+            <div class="content">Specifier votre choix en cliquant sur l'un des deux boutons ci-dessous</div>
+            <div class="ui two column stackable center aligned grid segment">
+                <div class="column">
+                    <div class="ui button blue">Gerer les documents</div>
+                </div>
+                <div class="ui vertical divider">ou</div>
+                <div class="column">
+                    <div class="teal ui button ">Gerer les clients</div>
+                </div>
             </div>
-            <?php 
+        </div>
+        <?php 
                 include("../../includes/footer.php");
             ?>
-        </div>
-    </div>
 
-    <script>
-        function offer() {
-            xhttp = new XMLHttpRequest();
-            xhttp.open("GET", "../traitements/eteindre.php", true);
-            xhttp.send();
-        }
-        function clickl() {
+        <script>
+            function offer() {
+                xhttp = new XMLHttpRequest();
+                xhttp.open("GET", "../traitements/eteindre.php", true);
+                xhttp.send();
+            }
+            function clickl() {
 
-            var menu = document.getElementById('mainmenu');
-            menu.style.minHeight = screen.height;
-        }
-    </script>
+                var menu = document.getElementById('mainmenu');
+                menu.style.minHeight = screen.height;
+            }
+        </script>
 </body>
